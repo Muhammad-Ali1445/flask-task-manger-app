6727869ed38c96df9558231e82b89b9d30e3bc7e
Muhammad Ali <alimunir1445@gmail.com>
2026-04-22 16:01:29 +0500

    add the login and register page along with its functionality & also add flask login manager

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+import enum
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime, date
-import enum
+from flask_login import UserMixin
+from extensions import login_manager
+from flask_login import login_user, current_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -11,14 +15,31 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.config["SECRET_KEY"] = "123456"
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager.init_app(app)
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     tasks = db.relationship("Task", backref="user", lazy=True)
 
 
@@ -33,9 +54,9 @@ class Task(db.Model):
     title = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Enum(TaskStatus), default=TaskStatus.PENDING, nullable=False)
-    due_date = db.Column(db.Date(), default=date.today(), nullable=False)
+    due_date = db.Column(db.Date(), default=date.today, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.now, nullable=False)
 
     @property
     def friendly_time(self):
@@ -47,19 +68,88 @@ def test():
     return "Testing Route"
 
 
+# ----- Authentication ---------
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for("get_all_tasks"))
+        else:
+            flash("Invalid username or password", "danger")
+
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        existing_user = User.query.filter(
+            (User.username == username) | (User.email == email)
+        ).first()
+        if existing_user:
+            flash("User already Exist", "danger")
+            return redirect(url_for("login"))
+
+        user = User(username=username, email=email, password="")
+        user.set_password(password)
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            flash("Account Created successfully! Please login", "success")
+            return redirect(url_for("login"))
+        except Exception as e:
+            db.session.rollback()
+            return f"Error {str(e)}", 500
+    return render_template("register.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Logout Successfully", "info")
+    return redirect(url_for("login"))
+
+
+# ----------- Tasks ------------
+
+
+# @app.route("/task/<int:id>", methods=["GET"])
+# def get_single_task(id):
+#     task = Task.query.get_or_404(id)
+#     return render_template("index.html", tasks=[task])
+
+
 @app.route("/task/<int:id>", methods=["GET"])
+@login_required
 def get_single_task(id):
-    task = Task.query.get_or_404(id)
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
     return render_template("index.html", tasks=[task])
 
 
+# -- tasks of logged in user
 @app.route("/tasks", methods=["GET"])
+@login_required
 def get_all_tasks():
-    all_tasks = Task.query.all()
+    all_tasks = Task.query.filter_by(user_id=current_user.id).all()
     return render_template("index.html", tasks=all_tasks)
 
 
 @app.route("/create_task", methods=["GET", "POST"])
+@login_required
 def create_task():
 
     if request.method == "POST":
@@ -72,7 +162,12 @@ def create_task():
             task_status = TaskStatus[status_str.upper()]
 
             # Create and Save Task
-            newTask = Task(title=title, description=description, status=task_status)
+            newTask = Task(
+                title=title,
+                description=description,
+                status=task_status,
+                user_id=current_user.id,
+            )
             db.session.add(newTask)
             db.session.commit()
 
@@ -88,8 +183,9 @@ def create_task():
 
 
 @app.route("/update/<int:id>", methods=["GET", "POST"])
+@login_required
 def update_task(id):
-    task = Task.query.get_or_404(id)
+    task = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
 
     if request.method == "POST":
         task.title = request.form.get("title")
@@ -108,8 +204,9 @@ def update_task(id):
 
 
 @app.route("/delete/<int:id>", methods=["POST"])
+@login_required
 def delete_task(id):
-    task_to_delete = Task.query.get_or_404(id)
+    task_to_delete = Task.query.filter_by(id=id, user_id=current_user.id).first_or_404()
 
     try:
         db.session.delete(task_to_delete)
@@ -118,6 +215,17 @@ def delete_task(id):
     except Exception as e:
         db.session.rollback()
         return f"Error deleting task:{str(e)}", 400
+
+
+@app.cli.command("create-admin")
+def create_admin():
+    """Custom command to create a default user."""
+    # This automatically handles the 'app context' for you!
+    user = User(username="ali", email="ali@gmail.com")
+    user.set_password("123456")
+    db.session.add(user)
+    db.session.commit()
+    print("User 'ali' created successfully!")
 
 
 if __name__ == "__main__":
